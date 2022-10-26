fb464a717ed06c3d777624c83c558e3ee2e0cd32
Aleksei Ivanov <profpopoff@gmail.com>
2022-10-26 16:09:43 +0300

    Inintioal commit

---
+++ dist/main.db96cdbfc6c120f9b24d.js
@@ -0,0 +1,91 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.html":
+/*!********************!*\
+  !*** ./index.html ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// Module
+var code = "<!DOCTYPE html>\r\n<html lang=\"en\">\r\n\r\n<head>\r\n   <meta charset=\"UTF-8\">\r\n   <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n   <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n   <title>Portfolio</title>\r\n</head>\r\n\r\n<body>\r\n   <h1>test</h1>\r\n   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt aliquam minus impedit, dolorem\r\n      perferendis saepe accusantium magni consequatur maxime tempora voluptatibus optio libero fugit numquam placeat\r\n      fuga facilis vitae?</p>\r\n</body>\r\n\r\n</html>";
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (code);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _index_html__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.html */ "./index.html");
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.db96cdbfc6c120f9b24d.js.map
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const mode = process.env.NODE_ENV || 'development'
+
+const devMode = mode === 'development'
+
+const target = devMode ? 'web' : 'browserslist'
+const devtool = devMode ? 'source-map' : undefined
+
+module.exports = {
+   mode,
+   target,
+   devtool,
+   context: path.resolve(__dirname, 'src'),
+   entry: './index.js',
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      filename: '[name].[contenthash].js',
+      assetModuleFilename: 'assets/[hash][ext]'
+   },
+   devServer: {
+      port: 4200,
+      hot: true
+   },
+   plugins: [
+      new HTMLWebpackPlugin({
+         template: './index.html'
+      })
+   ],
+   module: {
+      rules: [
+         {
+            test: /\.html$/i,
+            loader: 'html-loader'
+         },
+         {
+            test: /\.(c|sa|sc)ss$/i,
+            use: [
+               devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+               'css-loader',
+               {
+                  loader: 'postcss-loader',
+                  options: {
+                     postcssOptions: {
+                        plugins: [require('postcss-preset-env')]
+                     }
+                  }
+               },
+               'sass-loader'
+            ]
+         },
+         {
+            test: /\.(woff2?)$/i,
+            type: 'asset/resource',
+            generator: {
+               filename: 'fonts/[name][ext]'
+            }
+         },
+         {
+            test: /\.(png|jpe?g|webp|gif|svg)$/i,
+            type: 'asset/resource',
+            use: [
+               {
+                  loader: 'image-webpack-loader',
+                  options: {
+                     mozjpeg: {
+                        progressive: true,
+                     },
+                     optipng: {
+                        enabled: false,
+                     },
+                     pngquant: {
+                        quality: [0.65, 0.90],
+                        speed: 4
+                     },
+                     gifsicle: {
+                        interlaced: false,
+                     },
+                     webp: {
+                        quality: 75
+                     }
+                  }
+               }
+            ]
+         },
+         {
+            test: /\.m?js$/i,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: ['@babel/preset-env']
+               }
+            }
+         }
+      ]
+   }
+}
